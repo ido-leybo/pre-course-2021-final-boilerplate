594b44a9b57e70f5a45bbcdea2b44f69a81c3876
Ido Leybovitch <77382724+ido-leybo@users.noreply.github.com>
2021-01-28 14:00:36 -0800

    add sort function to main js file and other functions
    add sort button
    add delete button
    add function to do the sort and the delete

--- src/main.js
+++ src/main.js
@@ -1,75 +1,129 @@
-const button = document.getElementById("add-button");
+const addButton = document.getElementById("add-button");
 const sortButton = document.getElementById('sort-button');
-const deleteButton = document.createElement("button");
-deleteButton.classList.add("delete-button");
-deleteButton.innerText = "X";
+
+
+const inputText = document.getElementById('text-input');
+const viewSection = document.getElementById('viewSection'); 
+const selector = document.getElementById("priority-selector");
 const counter = document.getElementById('counter');
 let todoCounter = 0;
+let containerId = 0;
 counter.innerText = todoCounter;
 let todoListArr = [];
 let sortArr = [];
+
+// class Task {
+//     constructor(text, priority) {}
+// }
 //add a "add" button to add more tasks to the Todo list every "click"
-button.addEventListener("click", () => {
-    addContainer()
-    addTaskToView();
+addButton.addEventListener("click", () => {
+    addToContainer();
+    // addTaskToViewSection();
+    plusCounter();
+    
 });
     //add a done button to remove the task we done //need to work on
-deleteButton.addEventListener("click", event => {
-    deleteTaskView(event)
-}); 
-//need to work on
-sortButton.addEventListener("click", sortTasks());
 
-function addTaskToView() {
-    const viewSection = document.getElementById('viewSection');
+//need to work on
+sortButton.addEventListener("click", () => {
+    sortTasks()
+});
+function addTaskToViewSection() { 
     for (let task of todoListArr) {
-        viewSection.appendChild(task);
-    }
+        displayTask(task)
+        todoCounter++;
+        counter.innerText = todoCounter;
+    } 
+};
+function plusCounter() {
     todoCounter++
     counter.innerText = todoCounter;
-};
+}
 function deleteTaskView(event) {
     todoCounter--;
     counter.innerText = todoCounter;
-    let removeDiv = event.target.closest('.todo-container');
+    let removeDiv = event.target.parentElement;
+    let id = removeDiv.id;
+    console.log(todoListArr);
     removeDiv.remove();
+    todoListArr = todoListArr.filter((task) => task.id !== parseInt(id));
+    console.log(todoListArr);
 }
-function addContainer() {
-    const mainDiv = document.createElement('div');
-    mainDiv.classList.add('todo-container');
-    const divPriority = document.createElement('div');
-    divPriority.classList.add('todo-priority');
-    const selector = document.getElementById("priority-selector");
+function addToContainer() {
     let selectorValue = selector.value;
-    const divDate = document.createElement('div');
-    divDate.classList.add('todo-created-at');
-    const divText = document.createElement('div');
-    divText.classList.add('todo-text');
-    mainDiv.append(divPriority);
-    mainDiv.append(divDate);
-    mainDiv.append(divText);
-    divPriority.innerText = selectorValue;
-    divDate.innerText = getCurrentDate();
-    divText.innerText = input_text();
-    todoListArr.push(mainDiv);
+    let task = {
+        id: containerId,
+        priority: selectorValue,
+        text: input_text(),
+        date: getCurrentDate()
+    };
+    containerId++;
+    todoListArr.push(task);
+    displayTask(task);
 };
 function input_text() {
-    let inputText = document.getElementById('text-input').value;
-    document.getElementById('text-input').value = '';
-    document.getElementById('text-input').focus();
-    return inputText;
-}
+    let text = inputText.value;
+    inputText.value = '';
+    inputText.focus();
+    return text;
+};
 function getCurrentDate() {
     const date = new Date(); 
     let nDate = date.toISOString().split('T')[0] + ' ' + date.toTimeString().split(' ')[0];
     return nDate;
 };
-
-
 //need to work on
 function sortTasks() {
-
+    clean_presented_list();
+    todoListArr = todoListArr.sort((a, b) => b.priority - a.priority);
+    addTaskToViewSection()
+}
+function clean_presented_list() {
+    while(viewSection.firstChild){
+        viewSection.removeChild(viewSection.lastChild); // first it cleans the shown list;
+    }
+    todoCounter = 0;
+    counter.innerText = todoCounter;
+};
+function displayTask(task) {
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('todo-container');
+    mainDiv.setAttribute("id", `${task.id}`);
+    const divPriority = document.createElement('div');
+    divPriority.classList.add('todo-priority');
+    const divDate = document.createElement('div');
+    divDate.classList.add('todo-created-at');
+    const divText = document.createElement('div');
+    divText.classList.add('todo-text');
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.innerText = "X";
+    deleteButton.addEventListener("click", event => {
+        deleteTaskView(event)
+    }); 
+    divPriority.innerText = task.priority;
+    divDate.innerText = task.date;
+    divText.innerText = task.text;
+    mainDiv.append(deleteButton);
+    mainDiv.append(divPriority);
+    mainDiv.append(divDate);
+    mainDiv.append(divText);
+    console.log(mainDiv);
+    viewSection.appendChild(mainDiv);
 }
 
+
+
+
+function saveAsJson() {
+};
+function getJson() {
+    return JSON.parse(localStorage.getItem("tasks"));
+}
+async function LoadJsonBin() {
+    const response = await fetch('https://api.jsonbin.io/v3');
+    const text = await response.text();
+    const json = JSON.parse(text);
+};
 
 
