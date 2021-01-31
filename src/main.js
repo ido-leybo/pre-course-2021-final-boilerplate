47586ba11890fc4e911d77d8a1a5492a29ca6a7f
Ido Leybovitch <77382724+ido-leybo@users.noreply.github.com>
2021-01-30 20:19:28 -0800

    fixed jsonBin PUT and GET

--- src/main.js
+++ src/main.js
@@ -5,45 +5,45 @@ const viewSection = document.getElementById('viewSection');
 const selector = document.getElementById("priority-selector");
 const counter = document.getElementById('counter');
 const searchButton = document.getElementById("search-button"); 
-let todoCounter = 0;
+const loading = document.getElementById("loading");
+
+let tasksCounter = 0;
 let containerId = 0;
-counter.innerText = todoCounter;
 let todoListArr = [];
 let searchTodoArr = [];
 let idArr = [];
-const loading = document.getElementById("loading");
 let loadingPage = true;
 
+counter.innerText = tasksCounter;
 //add a "add" button to add more tasks to the Todo list every "click"
 addButton.addEventListener("click", () => {
     taskToList();
-    plusCounter();
-    
+    increaseTasksCounter();
 });
-searchButton.addEventListener("click", goTo);
+searchButton.addEventListener("click", searchToGo);
+//add a sort button to sort the list by priority value
+sortButton.addEventListener("click", sortTasks);
 
-function goTo() {
+function increaseTasksCounter() {
+    tasksCounter++
+    counter.innerText = tasksCounter;
+}
+function searchToGo() {
     let text = input_text();
     searchTodoArr = todoListArr.filter(task => task.text.includes(text));
     console.log(searchTodoArr);
     clean_presented_list();
     getAndShow(searchTodoArr);
 }
-//add a sort button to sort the list by priority value
-sortButton.addEventListener("click", sortTasks);
 function getAndShow(tasksToDisplay) { // get the list from the sort action or from the local storage and show it in the html
     if (tasksToDisplay[0]) {
         for (let task of tasksToDisplay) {     
             displayTask(task);
-            todoCounter++;
-            counter.innerText = todoCounter;
+            tasksCounter++;
+            counter.innerText = tasksCounter;
         }
     }return;
 };
-function plusCounter() {
-    todoCounter++
-    counter.innerText = todoCounter;
-}
 function taskToList() { // put every task in the main list
     let selectorValue = selector.value;
     const text = input_text();
@@ -58,12 +58,11 @@ function taskToList() { // put every task in the main list
     displayTask(task);
     todoListArr.push(task);
     jsonBinUpdateTask(todoListArr);
-    localStorage.setItem('my-todo', JSON.stringify(todoListArr));
+    // localStorage.setItem('my-todo', JSON.stringify(todoListArr));
 };
 function deleteTaskView(event) {  // delete a task from the list and from the local storage.
-    todoCounter--;
-    counter.innerText = todoCounter;
-    // window.localStorage.removeItem('my-todo');
+    tasksCounter--;
+    counter.innerText = tasksCounter;
     let removeDiv = event.target.parentElement;
     let id = removeDiv.id;
     removeDiv.remove();
@@ -71,8 +70,6 @@ function deleteTaskView(event) {  // delete a task from the list and from the lo
     todoListArr = todoListArr.filter((task) => task.taskId !== id);
     console.log(todoListArr)
     localStorage.setItem('my-todo', JSON.stringify(todoListArr));
-    // let taskToDelete = todoListArr.find(task => task.taskId === id);
-    // jsonBinDeleteTask(taskToDelete.id);
     jsonBinUpdateTask(todoListArr);
 }
 function input_text() {
@@ -95,8 +92,8 @@ function clean_presented_list() { // first it cleans the shown list before the s
     while(viewSection.firstChild){
         viewSection.removeChild(viewSection.lastChild); 
     }
-    todoCounter = 0;
-    counter.innerText = todoCounter;
+    tasksCounter = 0;
+    counter.innerText = tasksCounter;
 };
 function displayTask(task) { // put the task in div's and show them in the html
     const deleteButton = document.createElement("button");
@@ -142,26 +139,25 @@ function displayTask(task) { // put the task in div's and show them in the html
     viewSection.appendChild(mainDiv);
 };
 async function jsonBinUpdateTask(updatedtasks) {
-        const response = await fetch("https://api.jsonbin.io/v3/b/6012c8c99f55707f6dfd4278", {
+    const response = await fetch("https://api.jsonbin.io/v3/b/6012c8c99f55707f6dfd4278", {
         method: "PUT",
         headers: {
-                'Content-Type': "application/json",
-                'X-Master-Key': "$2b$10$trCW.rdQAELT6mq2K5yQE.oywgCXlAnA2tO3Ooj03jYKDLz6jo8f."             
-            },
-            body: JSON.stringify(updatedtasks)
-            })
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                return data
-            })
-            .catch(error => {
-                console.error(error)
-            });
+            'Content-Type': "application/json",
+            'X-Master-Key': "$2b$10$trCW.rdQAELT6mq2K5yQE.oywgCXlAnA2tO3Ooj03jYKDLz6jo8f."             
+        },
+        body: JSON.stringify({"my-todo": updatedtasks})
+        })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            return data
+        })
+        .catch(error => {
+            console.error(error)
+        });
     console.log(response);
 };
-
 async function jsonBinGetTasks() {
     try {
         const allTasks = await fetch("https://api.jsonbin.io/v3/b/6012c8c99f55707f6dfd4278", {
@@ -172,33 +168,18 @@ async function jsonBinGetTasks() {
         })
         const text = await allTasks.text();
         const json = JSON.parse(text);
-        return json.record;
-        // todoListArr = allTasks
+        return json.record["my-todo"];
     } catch (error) {
         console.error(error);
     };
 };
-
-// async function jsonBinDeleteTask(binId) {
-//     try {
-//         await fetch(`https://api.jsonbin.io/b/${binId}`, {
-//             method: "DELETE",
-//             headers: {
-//                 'secret-key': "$2b$10$trCW.rdQAELT6mq2K5yQE.oywgCXlAnA2tO3Ooj03jYKDLz6jo8f."
-//             },
-//         });
-//     } catch (error) {
-//         console.error(error);
-//     }
-// };
-
+//this event run first, before everything
 window.addEventListener("DOMContentLoaded", async (event) => {
-    // todoListArr = await jsonBinGetTasks();
+    todoListArr = await jsonBinGetTasks();
     if (todoListArr !== null) loading.remove();
     console.log(todoListArr);
-    // loadingPage = false;
-    let tasks = JSON.parse(localStorage.getItem('my-todo') || '[]');
-    todoListArr = tasks;
+    // let tasks = JSON.parse(localStorage.getItem('my-todo') || '[]');
+    // todoListArr = tasks;
     getAndShow(todoListArr);
 });
     
